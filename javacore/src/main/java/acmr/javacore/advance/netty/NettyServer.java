6782013cc8cdaf62426236ffcba3577e95a4a3e0
guoguo <leslie_guoguo@163.com>
2021-07-13 18:20:11 +0800

    netty test

---
+++ javacore/src/main/java/acmr/javacore/advance/netty/NettyServer.java
@@ -0,0 +1,57 @@
+package acmr.javacore.advance.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
+
+public class NettyServer {
+
+    private int port = 9999;
+
+    public NettyServer(int port) {
+        this.port = port;
+    }
+
+    public void start() throws Exception {
+        ByteBuf buf = Unpooled.unreleasableBuffer(Unpooled
+                .copiedBuffer("hello world\n", StandardCharsets.UTF_8));
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        EventLoopGroup group = new NioEventLoopGroup();
+
+        bootstrap.group(group)
+                .channel(NioServerSocketChannel.class)
+                .localAddress(new InetSocketAddress(port))
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+
+                    @Override
+                    protected void initChannel(SocketChannel ch) {
+                        ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+                            @Override
+                            public void channelActive(ChannelHandlerContext ctx) {
+                                ctx.writeAndFlush(buf.duplicate()).addListener(ChannelFutureListener.CLOSE);
+                            }
+                        });
+                    }
+                });
+        ChannelFuture future = bootstrap.bind().sync();
+        future.channel().closeFuture().sync();
+        group.shutdownGracefully().sync();
+    }
+
+    public static void main(String[] args) {
+        NettyServer server = new NettyServer(9999);
+        try {
+            server.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
