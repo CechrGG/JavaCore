acb4dc2da7fe35f26e81fd2765f6bf78e3db7fc4
guoguo <leslie_guoguo@163.com>
2021-11-30 21:27:01 +0800

    commit boot jpa test

---
+++ javacore/src/test/java/acmr/javacore/basic/wtf/JustTest.java
@@ -0,0 +1,13 @@
+package acmr.javacore.basic.wtf;
+
+import org.junit.Test;
+
+public class JustTest {
+    @Test
+    public void test() {
+        String a = null;
+        if(a != null) {
+
+        }
+    }
+}
